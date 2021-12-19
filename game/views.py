dc165da484c23466226bfd7a7800eccc6cdcf66a
AditiKamath <adikamath3@gmail.com>
2021-12-19 23:24:19 +0530

    logout added

--- game/views.py
+++ game/views.py
@@ -8,15 +8,18 @@ from django.contrib.auth import authenticate, login,logout
 # Create your views here.
 # register user 
 def register(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            messages.success(request,'Registration Successful')
-            form.save()
-            return HttpResponseRedirect('/login/')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserRegistrationForm(request.POST)
+            if form.is_valid():
+                messages.success(request,'Registration Successful')
+                form.save()
+                return HttpResponseRedirect('/login/')
+        else :
+            form =UserRegistrationForm()
+        return render(request, 'custom/register.html',{'form':form})
     else :
-        form =UserRegistrationForm()
-    return render(request, 'custom/register.html',{'form':form})
+        return HttpResponseRedirect('/profile/')
 
 # Login 
 def user_login(request):
@@ -34,7 +37,10 @@ def user_login(request):
     return render(request,'custom/user_login.html',{'form':form})
 
 def user_profile(request):
-    return render(request,'custom/user_profile.html')
+    if request.user.is_authenticated:
+        return render(request,'custom/user_profile.html',{'name': request.user.username})
+    else :
+        return HttpResponseRedirect('/login/')
 
 def user_logout(request):
     logout(request)
